95f0938ba99e1f930566375ec6b6ae84bbb458c2
Niklas Saers <niklas@saers.com>
2020-10-26 17:17:24 +0100

    Compile with frameworks
    Getting: Include of non-modular header inside framework module 'SSHTunnel’

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,11 @@
 LIBRESSL=3.2.2
 curl -iL --max-redirs 1 -o libressl.zip https://github.com/build-xcframeworks/libressl/releases/download/${LIBRESSL}/${LIBRESSL}.zip
 unzip libressl.zip
-mv ${LIBRESSL} libressl
+mv ${LIBRESSL} frameworks
+rm -R frameworks/libssl.xcframework/*/Headers # don't have double set headers
+
+git clone https://github.com/build-xcframeworks/libssh2
+cd libssh2
+bash libssh2.sh
+mv output/libssh2.xcframework ../frameworks
+cd ..
